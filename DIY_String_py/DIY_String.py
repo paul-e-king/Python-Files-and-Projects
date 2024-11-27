b4fbd39f47908d9c217744396cdb2c85258aced1
Mr. King <60671734+paul-e-king@users.noreply.github.com>
2024-11-27 15:52:21 -0500

    Create DIY_String.py

---
+++ DIY_String_py/DIY_String.py
@@ -0,0 +1,11 @@
+# Right now, this is the start of a made up String object
+class myChar {
+    __init__(self, ch):
+        self.ch = ch
+        self.next = None
+}
+
+class myString {
+    def __init__(self, inStr=""):
+        self.__myStr = 
+}
